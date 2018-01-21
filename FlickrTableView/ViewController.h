f74df9f2b3ed5b192d223b9aced17069f55187e0
Anton Poluyanov <poluyanov80@gmail.com>
2018-01-21 17:47:47 +0300

    Initial Commit

---
+++ FlickrTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrTableView
+//
+//  Created by Антон Полуянов on 21/01/2018.
+//  Copyright © 2018 Антон Полуянов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrTableView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FlickrTableView
+//
+//  Created by Антон Полуянов on 21/01/2018.
+//  Copyright © 2018 Антон Полуянов. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
